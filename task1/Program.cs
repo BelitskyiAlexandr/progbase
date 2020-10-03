b2d867cafdbbaaec217f7b02fd0afa37d23c9a31
Alexandr Belitskyi <belitskyi.alexandr@gmail.com>
2020-10-03 09:50:56 +0300

    Task1 done with gitignore

---
+++ task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+
+
+class Program
+{
+    static void Main()
+    {
+        //рядковий коментар
+        //WriteLine     System.Console.WriteLine
+        WriteLine("Hello my name is Alexandr Belitskyi!");
+        Console.WriteLine("I'm from КП-01");
+        System.Console.WriteLine("Works too!");
+        WriteLine(789);
+        Write(",");
+        WriteLine(3.141592);
+        //Console.WriteLine("I'm from КП-01");
+        /* 
+        блоковий коментар
+        */
+    }
+}
+
