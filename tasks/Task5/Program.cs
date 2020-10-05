b7b119dda77e76e08369b259926cf80b5495bcfd
Alexandr Belitskyi <belitskyi.alexandr@gmail.com>
2020-10-05 13:31:03 +0300

    task 5

---
+++ tasks/Task5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Enter num1: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter num2: ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter num3: ");
+            c = int.Parse(Console.ReadLine());
+
+            int min = Math.Min(a,b);
+            min = Math.Min(min,c);
+            int max = Math.Max(a,b);
+            max = Math.Max(max,c);
+            Console.WriteLine("Min: {0}.", min);
+            Console.WriteLine("Max: {0}.", max);
+
+        }
+    }
+}
