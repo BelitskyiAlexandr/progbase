edff8219152da8e75479bbc10da329cb69b52521
Alexandr Belitskyi <belitskyi.alexandr@gmail.com>
2020-10-05 13:00:44 +0300

    task 3

---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, num; 
+            Console.WriteLine("Enter a number: ");
+            num = int.Parse(Console.ReadLine());
+            a = (num / 100) % 10;
+            b = (num / 10) % 10;
+            c = num % 10;
+            int sum = a + c + b;
+            Console.WriteLine("Sum is {0}.", sum);
+        }
+    }
+}
