1a70e389424ffee41a0af43e6a85ce94a1cb1de6
Alexandr Belitskyi <belitskyi.alexandr@gmail.com>
2020-10-05 12:52:08 +0300

    task 2 sum aver

---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, sum;
+            double average;
+            Console.WriteLine("Enter a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine(a);
+            Console.WriteLine("Enter b: ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine(b);
+            Console.WriteLine("Enter c: ");
+            c = int.Parse(Console.ReadLine());
+            Console.WriteLine(c);
+
+
+            sum = a + b + c;
+            Console.WriteLine("Sum: " + sum);
+
+            average =(a+b+c)/3;
+            Console.WriteLine("Average: " + average);
+        }
+    }
+}
