775dddc3339103afcd2ef0461cffc8beee6921b4
Alexandr Belitskyi <belitskyi.alexandr@gmail.com>
2020-10-12 23:24:46 +0300

    lab1

---
+++ labs/lab1/part1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+using static System.Math;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Enter a: ");
+            double a = double.Parse(ReadLine());
+            WriteLine("Enter b: ");
+            double b = double.Parse(ReadLine());
+            WriteLine("Enter c: ");
+            double c = double.Parse(ReadLine());
+
+
+            double d0 = ((Pow((a+3), (c+1))-10)/(a-b));
+            double d1 = 4*b+(c/a);
+            double d2 = Pow((a+4), ((Abs(Sin(b)))/(1+c)));
+            double d = d0+d1+d2;
+            WriteLine("d0 = {0}.", d0);
+            WriteLine("d1 = {0}.", d1);
+            WriteLine("d2 = {0}.", d2);
+            WriteLine("d = {0}.", d);
+
+        }
+    }
+}
---
+++ labs/lab1/part2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using static System.Console;
+using static System.Math;
+
+namespace part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n;
+            double y;
+            WriteLine("Enter x: ");
+            double x = double.Parse(ReadLine());
+            n = (Pow(x, 2) - PI / 2) / PI;
+            if ((x > -6 && x < 6) || (x >= 8 && x <= 10))
+            {
+                if (x != 0.25) 
+                {
+                    y = (-2 / (4 * x - 1) - 1);
+                }
+                else
+                {
+                    y = double.NaN;
+                }
+            }
+            else
+            {
+                if (n % Floor(n) != 0) 
+                {
+                    y = Tan(Pow(x, 2)) + Pow(Sin(2 * x), 2);
+                }
+                else
+                {
+                    y = double.NaN;
+                }
+            }
+            WriteLine("y = {0}.", y);
+            
+        }
+    }
+}
