b9d60d2d6a2c1c48166c0aede63f2b0c271b8e31
BelitskyiAlexandr <vilisapett@gmail.com>
2020-11-19 02:12:32 +0200

    lab 3 done

--- labs/lab3/part2/Program.cs
+++ labs/lab3/part2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using static System.Console;
+using System.Linq;
 
 namespace part2
 {
@@ -7,22 +8,124 @@ namespace part2
     {
         static void Main(string[] args)
         {
-            Write("Enter size of territory (number of lines  number of columns): ");
-            string line = Console.ReadLine();
-            string[] data = line.Split(' ');
-            int n = int.Parse(data[0]);                 //first integer
-            int m = int.Parse(data[1]);                 //second integer
-            Random random = new Random();
-            int[,] a = new int[n, m];
+            WriteLine("**Enter size of territory**");
+            Write("Enter length of territory: ");
+            string Str1 = ReadLine();
+            int n;
+            bool isNum1 = int.TryParse(Str1, out n);
+            if (isNum1)
+            {
+                Write("Enter width of territory: ");
+                string Str2 = ReadLine();
+                int m;
+                bool isNum2 = int.TryParse(Str2, out m);
+                WriteLine();
+                if (isNum2)
+                {
+                    Random random = new Random();
+                    int[,] a = new int[n, m];
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < m; j++)
+                        {
+                            a[i, j] = random.Next(0, 2);
+                            Write(" {0}", a[i, j]);
+                        }
+                        WriteLine();
+                    }
+                    int[,] b = new int[a.GetLength(0), a.GetLength(1)];
+                    Array.Copy(a, b, a.GetLength(0) * a.GetLength(1));
+
+                    int islands = 0;
+                    foreach (int i in Enumerable.Range(0, a.GetLength(0)))
+                    {
+                        foreach (int j in Enumerable.Range(0, a.GetLength(1)))
+                        {
+                            if (Land(a, i, j))
+                                islands++;
+                        }
+                    }
+                    WriteLine("Number of islands is: {0}", islands);
+
+
+                    Picture(b, n, m);
+                }
+                else
+                {
+                    WriteLine("Please, enter integer number");
+                }
+            }
+            else
+            {
+                WriteLine("Please, enter integer number");
+            }
+        }
+
+        static bool Land(int[,] a, int i, int j)
+        {
+            if ((i < 0) || (i >= a.GetLength(0))) return false;
+            if ((j < 0) || (j >= a.GetLength(1))) return false;
+
+            bool island = a[i, j] == 1;
+
+            a[i, j] = 0;
+
+            if (island)
+            {
+                Land(a, i, j + 1);
+                Land(a, i, j - 1);
+                Land(a, i + 1, j);
+                Land(a, i - 1, j);
+            }
+
+            return island;
+        }
+
+        static void Picture(int[,] b, int n, int m)
+        {
+            for (int i = 0; i < m + 2; i++)
+            {
+                if ((i == 0) || (i == m + 1))
+                {
+                    Write("+");
+                }
+                else
+                {
+                    Write("-");
+                }
+            }
+            WriteLine();
+
             for (int i = 0; i < n; i++)
             {
+                Write("|");
                 for (int j = 0; j < m; j++)
                 {
-                    a[i,j] = random.Next(0,2);
-                    Write(a[i,j]);
+                    if (b[i, j] == 1)
+                    {
+                        Write("N");
+                    }
+                    else
+                    {
+                        Write(" ");
+                    }
                 }
-            WriteLine();
+                Write("|");
+                WriteLine();
             }
+
+            for (int i = 0; i < m + 2; i++)
+            {
+                if ((i == 0) || (i == m + 1))
+                {
+                    Write("+");
+                }
+                else
+                {
+                    Write("-");
+                }
+            }
+            WriteLine();
         }
     }
 }
---
+++ tasks/any/vadran/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+
+namespace vadran
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = 40;
+            WriteLine("Enter k: ");
+            int k = int.Parse(ReadLine());
+            for (int i = 1; i<= m; i++)
+            {
+                Write("-");
+                if ( i % k == 0)
+                {
+                    WriteLine();
+                }
+            }
+        }
+    }
+}
