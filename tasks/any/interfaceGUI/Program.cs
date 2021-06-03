9860e8c17c9eba960adbba3f1a3670ee72c0444e
BelitskyiAlexandr <vilisapett@gmail.com>
2021-06-03 05:40:35 +0300

    ex-import

--- tasks/any/interfaceGUI/Program.cs
+++ tasks/any/interfaceGUI/Program.cs
@@ -15,6 +15,7 @@ namespace interfaceGUI
             UserRepository userRepository = new UserRepository(connection);
             GoodRepository goodRepository = new GoodRepository(connection);
             OrderRepository orderRepository = new OrderRepository(connection);
+            XmlProcess xmlProcess = new XmlProcess();
 
             Application.Init();
 
@@ -44,6 +45,7 @@ namespace interfaceGUI
                 {
                     ModeratorHomeWindow homeWindow = new ModeratorHomeWindow();
                     homeWindow.SetRepository(enteringWin.loggedUser, userRepository, goodRepository, orderRepository);
+                    homeWindow.SetXml(xmlProcess);
                     top.RemoveAll();
                     top.Add(homeWindow);
                     Application.Run();
@@ -51,21 +53,6 @@ namespace interfaceGUI
     
                 }
             }
-
-            // User user = userRepository.GetUserByUsername("tester");
-            // Order order = new Order();
-            // Good good1 = goodRepository.GetById(11);
-            // Good good2 = goodRepository.GetById(12);
-
-            // order = new Order(user.id);
-            // order.AddGoodToOrder(good1);
-            // order.AddGoodToOrder(good2);
-            // order.SetAmount();
-            // // long id = orderRepository.Insert(order);
-            // order.id = 12;
-
-            // orderRepository.AddGoodsOrders(order);
-
         }
     }
 }
