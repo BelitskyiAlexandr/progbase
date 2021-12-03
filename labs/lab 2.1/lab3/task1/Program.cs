bc61fd13f778935b4216e844f5e50d87951bad24
BelitskyiAlexandr <vilisapett@gmail.com>
2021-12-03 12:08:59 +0200

    lab3

---
+++ labs/lab 2.1/lab3/task1/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Group group1 = new Group("First");
+        group1.Add(new Student("Igor"));
+        group1.Add(new Student("Nata"));
+        group1.Add(new Student("Georg"));
+
+        group1.ShowInfo(1);
+        group1.PassExam();
+        Console.WriteLine();
+
+        group1.ShowInfo(1);
+
+        Console.WriteLine("=====");
+
+        Group group2 = new Group("Second");
+        Student gerd = new Student("Gerd");
+
+        group2.Add(gerd);
+        group2.Add(new Student("Bob"));
+        group2.Add(new Student("Pidge"));
+
+        group2.ShowInfo(1);
+        gerd.PassExam();
+        Console.WriteLine();
+
+        group2.ShowInfo(1);
+    }
+}
+
+abstract class Component //основа
+{
+    protected string _name;
+    protected bool _passExam;
+
+    public Component(string name)
+    {
+        this._name = name;
+        this._passExam = false;
+    }
+
+    public abstract void ShowInfo(int indent);
+    public abstract bool PassExam();
+    public abstract void Add(Component a);
+    public abstract void Remove(Component a);
+}
+
+class Student : Component
+{
+    public Student(string name) : base(name) { }
+
+    public override void ShowInfo(int indent)
+    {
+        Console.WriteLine(new String('-', indent) + "+ " + _name + "\t" + _passExam);
+    }
+    public override bool PassExam()
+    {
+        return this._passExam = true;
+    }
+
+    public override void Add(Component student)
+    {
+        Console.WriteLine("Cannot add to student");
+    }
+
+
+    public override void Remove(Component student)
+    {
+        Console.WriteLine("Cannot remove from student");
+    }
+}
+
+class Group : Component  //компонувальник
+{
+    private List<Component> students = new List<Component>();
+
+    public Group(string name) : base(name) { }
+
+    public override void Add(Component a)
+    {
+        students.Add(a);
+    }
+
+    public override void Remove(Component a)
+    {
+        students.Remove(a);
+    }
+
+    public override void ShowInfo(int indent)
+    {
+        Console.WriteLine(new String('-', indent) + "+ " + _name + "\t" + _passExam);
+
+        foreach (var student in students)
+        {
+            student.ShowInfo(indent + 2);
+        }
+    }
+
+    private void IfPassedAllGroup()
+    {
+        bool ifPassedAllGroup = true;
+        foreach (var student in students)
+        {
+            if (!student.PassExam())
+            {
+                ifPassedAllGroup = false;
+            }
+        }
+        _passExam = ifPassedAllGroup;
+    }
+
+    public override bool PassExam()
+    {
+        IfPassedAllGroup();
+        foreach (var student in students)
+        {
+            student.PassExam();
+        }
+        return true;
+    }
+}
+
---
+++ labs/lab 2.1/lab3/task2/Program.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Module module1 = new Module("Module 1");
+        // module1.Display();
+        // module1.MakeGreatMarks();
+        // module1.Display();
+
+        //Student student = new Student("Gerd");
+
+
+        ProxyStudent student = new ProxyStudent("Gerd");
+        student.DisplayModules();
+        Console.WriteLine();
+
+        student.DoNextModule();
+        Console.WriteLine();
+
+        student.DoGreatPrevousModuls();
+        student.DisplayModules();
+
+        student.DoNextModule();
+        Console.WriteLine();
+        student.DisplayModules();
+    }
+}
+
+abstract class UnivetsityHuman
+{
+    protected string _name;
+    protected int currentmodule;
+    protected List<Module> modules;
+
+    public UnivetsityHuman(string name)
+    {
+        this._name = name;
+        this.currentmodule = 1;
+        modules = new List<Module>();
+        for (int i = 0; i < 2; i++)
+        {
+            modules.Add(new Module($"Module {i + 1}"));
+        }
+    }
+    public abstract void DoNextModule();
+    public abstract void DoGreatPrevousModuls();
+    public abstract void DisplayModules();
+}
+
+class ProxyStudent : UnivetsityHuman
+{
+    Student student;
+    public ProxyStudent(string name) : base(name)
+    {
+        student = new Student(name);
+        this.modules = student.Modules;
+    }
+
+    public override void DisplayModules()
+    {
+        student.DisplayModules();
+    }
+
+    public override void DoGreatPrevousModuls()
+    {
+        student.DoGreatPrevousModuls();
+    }
+
+    private bool IfCanDoNextModule()
+    {
+        if (modules[currentmodule - 1].CheckBadMark())
+            return false;
+        return true;
+    }
+    public override void DoNextModule()
+    {
+        if (IfCanDoNextModule())
+        {
+            student.DoNextModule();
+            Console.WriteLine("Next module done");
+        }
+        else
+            Console.WriteLine("You should do previous modules great (>= 10) ");
+    }
+}
+class Student : UnivetsityHuman
+{
+    public Student(string name) : base(name) { }
+
+    public List<Module> Modules
+    {
+        get { return this.modules; }
+    }
+    public override void DisplayModules()
+    {
+        foreach (var item in modules)
+        {
+            Console.WriteLine($"{item.Name}");
+            item.Display();
+            Console.WriteLine();
+        }
+    }
+
+    public override void DoGreatPrevousModuls()
+    {
+        for (int i = 0; i < currentmodule; i++)
+        {
+            modules[i].MakeGreatMarks();
+        }
+    }
+
+    public override void DoNextModule()
+    {
+        currentmodule++;
+        modules[currentmodule - 1].MakeRandomMarks();
+    }
+}
+
+class Module
+{
+    private List<Exercise> _exercises;
+    private string _name;
+
+    public string Name
+    {
+        get { return _name; }
+    }
+    public Module(string name)
+    {
+        this._name = name;
+        this._exercises = GenerateExercises();
+    }
+
+    private List<Exercise> GenerateExercises()
+    {
+        Random rnd = new Random();
+        List<Exercise> exercises = new List<Exercise>();
+        string[] randomExercises = { "Math", "PE", "IT", "Biology", "History", "English", "Literature" };
+
+        string[] randomFirstFour = randomExercises.OrderBy(x => rnd.Next()).ToArray();
+
+        for (int i = 0; i < 4; i++)
+        {
+            Exercise ex = new Exercise(randomFirstFour[i]);
+            exercises.Add(ex);
+        }
+        return exercises;
+    }
+
+    public void Display()
+    {
+        foreach (var item in _exercises)
+        {
+            Console.WriteLine($"{item.Name,-15}\t{item.Mark,4}");
+        }
+    }
+
+    public void MakeGreatMarks()
+    {
+        Random rnd = new Random();
+        foreach (var item in _exercises)
+        {
+            item.ChangeMark(rnd.Next(10, 13));
+        }
+    }
+
+    public void MakeRandomMarks()
+    {
+        Random rnd = new Random();
+        foreach (var item in _exercises)
+        {
+            item.ChangeMark(rnd.Next(1, 13));
+        }
+    }
+
+    public bool CheckBadMark()
+    {
+        foreach (var item in _exercises)
+        {
+            if (item.Mark < 10)
+                return true;
+        }
+        return false;
+    }
+
+}
+
+class Exercise
+{
+    private string _name;
+    private int _mark;
+
+    public Exercise(string name)
+    {
+        this._name = name;
+        //Random rnd = new Random();
+        //this._mark = rnd.Next(1,10);
+        this._mark = 0;
+    }
+
+    public void ChangeMark(int mark)
+    {
+        if (mark > 12 || mark < 0)
+            throw new ArgumentException("Error: mark must be from 0 to 12");
+        this._mark = mark;
+    }
+
+    public string Name
+    {
+        get { return _name; }
+    }
+
+    public int Mark
+    {
+        get { return _mark; }
+    }
+}
