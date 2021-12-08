949b4ded3b2468170698690dd3be4141f15f18ba
BelitskyiAlexandr <vilisapett@gmail.com>
2021-12-08 21:54:19 +0200

    lab 5

---
+++ labs/lab 2.1/lab5/task1(strategy)/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Human human = new Human(new Currency());
+        human.GiveMoney();
+        human.setMoney = new Bank();
+        human.GiveMoney();
+        human.setMoney = new Shares();
+        human.GiveMoney();
+    }
+}
+
+interface ISetMoney
+{
+    void PayFor();
+}
+
+class Currency : ISetMoney
+{
+    public void PayFor()
+    {
+        Console.WriteLine("Currency was purchased");
+    }
+}
+
+class Bank : ISetMoney
+{
+    public void PayFor()
+    {
+        Console.WriteLine("Money was put on deposit");
+    }
+}
+
+class Shares : ISetMoney
+{
+    public void PayFor()
+    {
+        Console.WriteLine("Shares was purchased");
+    }
+}
+class Human
+{
+    public Human(ISetMoney setMoney)
+    {
+        this.setMoney = setMoney;
+    }
+    public ISetMoney setMoney { private get; set; }
+
+    public void GiveMoney()
+    {
+        setMoney.PayFor();
+    }
+}
---
+++ labs/lab 2.1/lab5/task2(chainOfResposibility)/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        StudentWork studentWork = new StudentWork(false);
+
+        Checker teacher1 = new Teacher("Gerd", true);
+        Checker teacher2 = new Teacher("Asya", false);
+        teacher1.Successor = teacher2;
+
+        teacher1.Handle(studentWork);
+    }
+}
+
+abstract class Checker
+{
+    public Checker Successor { get; set; }
+    public abstract void Handle(StudentWork studentWork);
+}
+
+class Teacher : Checker
+{
+    private string _name;
+    public bool busy { get; private set; }
+
+    public Teacher(string name, bool busy)
+    {
+        this._name = name;
+        this.busy = busy;
+    }
+
+    public override void Handle(StudentWork studentWork)
+    {
+        if (studentWork.isChecked == false && this.busy == false)
+        {
+            Console.WriteLine("Student work has been checked by " + this._name);
+            studentWork.isChecked = true;
+        }
+        else if (Successor != null)
+        {
+            Console.WriteLine($"{this._name} is busy. Work has gone to next teacher");
+            Successor.Handle(studentWork);
+        }
+    }
+}
+
+class StudentWork
+{
+    public bool isChecked;
+
+    public StudentWork(bool isChecked)
+    {
+        this.isChecked = isChecked;
+    }
+}
+
