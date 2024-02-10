32226c15e09883b25b845df2fd00a00e18185f4a
BelitskyiAlexandr <vilisapett@gmail.com>
2024-02-10 17:25:05 +0200

    try of search

--- MOIR/lab1/main.py
+++ MOIR/lab1/main.py
@@ -25,3 +25,30 @@ def read_documents_from_directory(directory_path):
 directory_path = input("Enter path to derictory with docs: ")
 documents = read_documents_from_directory(directory_path)
 print("Number of docs:", len(documents))
+
+#search
+def execute_search_query(index_terms, documents):
+    while True:
+        query = input("Enter a search term or 'exit' to exit: ")
+        if query.lower() == 'exit':
+            break
+        query_terms = query.split()  # Splitting a query into terms
+        relevant_documents = []
+        for i, document in enumerate(documents):
+            relevant_terms = []
+            for term in query_terms:
+                if term in index_terms and term in document:
+                    relevant_terms.append(term)
+            if relevant_terms:
+                relevant_documents.append((i + 1, relevant_terms, document))
+        if relevant_documents:
+            print("Relevant documents for the request '{}':".format(query))
+            for doc_info in relevant_documents:
+                print("Document №{}:".format(doc_info[0]))
+                print("Relevant terms:", doc_info[1])
+                print("Content of the document:", doc_info[2])
+        else:
+            print("There is no relevant documents for the query '{}'.\n".format(query)) 
+
+
+execute_search_query(index_terms, documents)
