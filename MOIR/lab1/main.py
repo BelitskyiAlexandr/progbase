384355e33eaa862774b5a19e80d1c68e6aa7b4b3
BelitskyiAlexandr <vilisapett@gmail.com>
2024-02-10 14:52:48 +0200

    step 2: docs for exe lab

--- MOIR/lab1/main.py
+++ MOIR/lab1/main.py
@@ -1,4 +1,4 @@
-
+# index terms
 def read_index_terms_from_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         index_terms = file.readlines()
@@ -8,3 +8,20 @@ def read_index_terms_from_file(file_path):
 file_path = input("Enter path to the file with index terms: ")
 index_terms = read_index_terms_from_file(file_path)
 print("Set of the index terms:", index_terms)
+
+#docs
+import os
+
+def read_documents_from_directory(directory_path):
+    documents = []
+    for filename in os.listdir(directory_path):
+        if filename.endswith('.txt'):
+            with open(os.path.join(directory_path, filename), 'r') as file:
+                document_content = file.read()
+                documents.append(document_content)
+    return documents
+
+
+directory_path = input("Enter path to derictory with docs: ")
+documents = read_documents_from_directory(directory_path)
+print("Number of docs:", len(documents))
