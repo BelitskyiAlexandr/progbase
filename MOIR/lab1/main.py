fa043a64c149e7ab84f3142fd5d9fd8b0f6f1a16
BelitskyiAlexandr <vilisapett@gmail.com>
2024-02-15 01:16:10 +0200

    try to add 'not included' term

--- MOIR/lab1/main.py
+++ MOIR/lab1/main.py
@@ -29,7 +29,63 @@ documents = read_documents_from_directory(directory_path)
 print("Number of docs:", len(documents))
 
 #search
+'''
+import re
 
+# Функція для виконання пошукового запиту
+def execute_search_query(index_terms, documents):
+    while True:
+        query = input("Enter the search query or 'exit' to quit: ")
+        if query.lower() == 'exit':
+            break
+        
+        # Розбиваємо запит на диз'юнкції
+        disjunctions = query.split('^')
+        
+        relevant_documents = set()
+        for disjunction in disjunctions:
+            conjunctions = disjunction.split('*')
+            relevant_docs_for_disjunction = set(range(1, len(documents) + 1))
+            for conjunction in conjunctions:
+                # Розділяємо терми на терми з відсутністю (!) та без
+                terms_with_negation = re.findall(r'(!?\w+)', conjunction)
+                required_terms = set()
+                excluded_terms = set()
+                for term in terms_with_negation:
+                    if term.startswith('!'):
+                        excluded_terms.add(term[1:])  # Відсутні терми
+                    else:
+                        required_terms.add(term)  # Терми, які повинні бути в документі
+                # Виконуємо пошук для кожної частини диз'юнкції
+                relevant_docs_for_conjunction = set()
+                for i, document in enumerate(documents):
+                    # Перевіряємо, чи всі терми, які мають бути в документі, присутні
+                    if all(term in document for term in required_terms):
+                        # Перевіряємо, чи всі терми, яких не повинно бути в документі, відсутні
+                        if not any(term in document for term in excluded_terms):
+                            relevant_docs_for_conjunction.add(i + 1)
+                relevant_docs_for_disjunction &= relevant_docs_for_conjunction
+            relevant_documents |= relevant_docs_for_disjunction
+        
+        # Виводимо результати пошуку для кожної диз'юнкції
+        if relevant_documents:
+            print("Relevant documents for the query '{}':".format(query))
+            for doc_index in relevant_documents:
+                print("Document #{}:".format(doc_index))
+                print("Relevant terms:", query)
+                print("Document content:", documents[doc_index - 1])
+        else:
+            print("No relevant documents found for the query '{}'.\n".format(query))
+
+
+# Приклад виклику функції для виконання пошукових запитів
+execute_search_query(index_terms, documents)
+
+
+
+
+
+'''
 import re
 
 # Функція для виконання пошукового запиту
@@ -68,3 +124,4 @@ def execute_search_query(index_terms, documents):
 
 # Приклад виклику функції для виконання пошукових запитів
 execute_search_query(index_terms, documents)
+
