f15dd04392e89458839a65dd58d5704f6bd900f6
BelitskyiAlexandr <vilisapett@gmail.com>
2024-02-15 00:03:45 +0200

    try search

--- MOIR/lab1/main.py
+++ MOIR/lab1/main.py
@@ -31,51 +31,38 @@ print("Number of docs:", len(documents))
 #search
 import re
 
-# Функція для розбиття пошукового запиту у НДФ на окремі терми
-def split_ndf_query(query):
-    query_terms = re.split(r'[\s^*]+', query)  # Розділяємо запит за символами '^' та '*'
-    return query_terms
-
-# Функція для виконання пошуку за НДФ у документах
-def execute_ndf_search_query(index_terms, documents):
+def execute_search_query(index_terms, documents):
     while True:
-        query = input("Введіть пошуковий запит у НДФ або 'exit' для виходу: ")
+        query = input("Введіть пошуковий запит або 'exit' для виходу: ")
         if query.lower() == 'exit':
             break
-        query_terms = split_ndf_query(query)
+        query_terms = re.split(r'[\s^*]+', query)  # Розділити запит на терми без '^' та '*'
         relevant_documents = []
         for i, document in enumerate(documents):
             relevant_terms = []
-            is_valid = True  # Змінна, що вказує на валідність документу за умовою
-            for term in query_terms:
-                if term.startswith('^'):  # Для диз'юнкції
-                    term = term[1:]  # Видаляємо символ '^'
-                    if term not in index_terms or term not in document:
-                        is_valid = False
-                        break
-                elif term.startswith('*'):  # Для кон'юнкції
-                    term = term[1:]  # Видаляємо символ '*'
-                    if term not in index_terms or term not in document:
-                        is_valid = False
-                        break
-                else:  # Якщо немає спеціального символу, просто перевіряємо наявність терму у документі
-                    if term not in index_terms or term not in document:
-                        is_valid = False
-                        break
-            if is_valid:
-                relevant_documents.append((i + 1, document))
+            conjunction_groups = query.split('^')  # Розділити запит на групи кон'юнкцій
+            is_relevant = False
+            for group in conjunction_groups:
+                terms = re.split(r'[\s*]+', group)  # Розділити групу на терми без '*'
+                if all(term in document for term in terms):
+                    relevant_terms.extend(terms)
+                    is_relevant = True
+            if is_relevant:
+                relevant_documents.append((i + 1, relevant_terms, document))
 
         if relevant_documents:
             print("\nРелевантні документи для запиту '{}':".format(query))
             for doc_info in relevant_documents:
                 print("---\nДокумент №{}:".format(doc_info[0]))
-                print("Вміст документу:\n", doc_info[1])
+                print("Релевантні терми:", doc_info[1])
+                print("Вміст документу:\n", doc_info[2])
                 print('---\n')
         else:
             print("Немає релевантних документів для запиту '{}'.\n".format(query))
 
-# Приклад виклику функції для виконання пошукового запиту
-execute_ndf_search_query(index_terms, documents)
+# Приклад виклику функції для виконання пошукових запитів
+execute_search_query(index_terms, documents)
+
 
 '''
 def execute_search_query(index_terms, documents):
