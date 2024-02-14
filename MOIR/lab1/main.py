5488ceee77ba8238bc4e3c6239ea3f9342b8c673
BelitskyiAlexandr <vilisapett@gmail.com>
2024-02-15 00:47:17 +0200

    try search

--- MOIR/lab1/main.py
+++ MOIR/lab1/main.py
@@ -29,51 +29,35 @@ documents = read_documents_from_directory(directory_path)
 print("Number of docs:", len(documents))
 
 #search
+# Функція для виконання пошукового запиту в НДФ
+def execute_boolean_query(index_terms, documents, query):
+    # Розділити запит на окремі терми та оператори
+    query_terms = query.split()
+    
+    # Цикл для пошуку по кожному терму або терміну
+    relevant_documents = set(range(1, len(documents) + 1))  # Початково всі документи вважаємо релевантними
+    for term_or_term in query_terms:
+        if '*' in term_or_term:
+            # Розділити терм на окремі літерали (за оператором *)
+            literals = term_or_term.split('*')
+            # Знайти документи, що містять всі терми
+            relevant_documents &= {i + 1 for i, document in enumerate(documents) if all(literal in document for literal in literals)}
+        elif term_or_term in index_terms:
+            # Знайти документи, що містять даний терм
+            relevant_documents &= {i + 1 for i, document in enumerate(documents) if term_or_term in document}
+    
+    # Вивести релевантні терми та зміст документів
+    for doc_index in relevant_documents:
+        print("Документ №{}:".format(doc_index))
+        print("Релевантні терми:", [term for term in query_terms if term in index_terms])
+        print("Зміст документу:", documents[doc_index - 1])
 
-def execute_search_query(index_terms, documents):
-    while True:
-        query = input("Введіть пошуковий запит у НДФ або 'exit' для виходу: ")
-        if query.lower() == 'exit':
-            break
-        
-        # Парсинг запиту у НДФ
-        query_terms = parse_ndf_query(query)
-        
-        relevant_documents = []
-        for i, document in enumerate(documents):
-            relevant_terms = []
-            for term_group in query_terms:
-                # Перевірка, чи всі терми у групі знаходяться у документі
-                if all(term in document for term in term_group):
-                    relevant_terms.extend(term_group)
-            if relevant_terms:
-                relevant_documents.append((i + 1, relevant_terms, document))
-        
-        # Блок виведення результатів
-        if relevant_documents:
-            print("\nРелевантні документи для запиту '{}':".format(query))
-            for doc_info in relevant_documents:
-                print("---\nДокумент №{}:".format(doc_info[0]))
-                print("Релевантні терми:", doc_info[1])
-                print("Вміст документу:\n", doc_info[2])
-                print('---\n')
-        else:
-            print("Немає релевантних документів для запиту '{}'.\n".format(query)) 
+# Приклад виклику функції для виконання пошукового запиту
+query = input("Введіть пошуковий запит в НДФ: ")
+execute_boolean_query(index_terms, documents, query)
 
-# Функція для розбиття запиту у НДФ на окремі групи термів
-def parse_ndf_query(query):
-    # Розділення запиту за диз'юнкціями
-    disjunctions = query.split('^')
-    query_terms = []
-    for disjunction in disjunctions:
-        # Розділення диз'юнкції за кон'юнкціями
-        conjunctions = disjunction.split('*')
-        # Додавання кон'юнкцій до групи термів
-        query_terms.append([term.strip() for term in conjunctions])
-    return query_terms
 
-# Приклад використання функції для пошуку за НДФ
-execute_search_query(index_terms, documents)
+
 
 
 
