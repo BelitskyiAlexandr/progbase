4dd42c6505984dc977e07b29d744ed78611ff80c
BelitskyiAlexandr <vilisapett@gmail.com>
2024-03-24 16:07:51 +0200

    term + range

--- MOIR/lab3/main.py
+++ MOIR/lab3/main.py
@@ -37,13 +37,38 @@ def read_documents():
 def delete_document(doc_id):
     es.delete(index=index_name, id=doc_id)
 
+def filter_documents(field, value):
+    if field == "date" or field == "revenue":
+        start, end = map(int, value.split())
+        query = {
+            "query": {
+                "range": {
+                    field: {
+                        "gte": start,
+                        "lte": end
+                    }
+                }
+            }
+        }
+    else:
+        query = {
+            "query": {
+                "term": {
+                    field: value
+                }
+            }
+        }
+    results = es.search(index=index_name, body=query)
+    return results['hits']['hits']
+
 
 while True:
     print("\nOptions:")
     print("1. Create a new document")
     print("2. Read all documents")
-    print("3. Delete a document")
-    print("4. Exit")
+    print("3. Filter documents")
+    print("4. Delete a document")
+    print("5. Exit")
     choice = input("Enter your choice: ")
 
     if choice == '1':
@@ -66,11 +91,24 @@ while True:
             print("")
 
     elif choice == '3':
+        field = input("Enter field to filter by (name, date, participants, revenue): ")
+        value = input("Enter value to filter by (for range use format start end): ")
+        filtered_documents = filter_documents(field, value)
+        print("\nFiltered Documents:")
+        for doc in filtered_documents:
+            print("ID:", doc['_id'])
+            print("Name:", doc['_source']['name'])
+            print("Date:", doc['_source']['date'])
+            print("Participants:", doc['_source']['participants'])
+            print("Revenue:", doc['_source']['revenue'])
+            print("")
+
+    elif choice == '4':
         doc_id = input("Enter document ID to delete: ")
         delete_document(doc_id)
         print("Document deleted successfully!")
 
-    elif choice == '4':
+    elif choice == '5':
         break
 
     else:
